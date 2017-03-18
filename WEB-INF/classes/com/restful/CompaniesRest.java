cb8409ed7d98b17e77f2030d87218fa1b82da94f
System Administrator <root@Kenneths-MacBook-Pro.local>
2017-03-18 16:47:54 -0400

    Renamed files, and added GET to /companies api.

--- WEB-INF/classes/com/restful/CompaniesRest.java
+++ WEB-INF/classes/com/restful/CompaniesRest.java
@@ -11,81 +11,64 @@ import java.io.InputStreamReader;
 import java.io.InputStream;
 import java.io.IOException;
 
-// Plain old Java Object it does not extend as class or implements
-// an interface
-
-// The class registers its methods for the HTTP GET request using the @GET annotation.
-// Using the @Produces annotation, it defines that it can deliver several MIME types,
-// text, XML and HTML.
-
-// The browser requests per default the HTML MIME type.
-
-//Sets the path to base URL + /companies/{symbol}
-@Path("/companies/{symbol}")
+@Path("/companies")
     public class CompaniesRest {
 
 	@GET
 	    @Produces(MediaType.APPLICATION_JSON)
-	    public Response getCompany( @PathParam("symbol") String sym ) {
-
-	    com.simulator.Company c = com.simulator.Company.getCompanyBySymbol(sym);
-	    if(c == null) {
-		return Response.status(Response.Status.NOT_FOUND).build();
+	    public Response getCompanies() {
+	    com.simulator.Company[] companies = com.simulator.Company.getCompanies();
+	    if(companies == null)
+		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+	    
+	    String json = "[\n";
+	    for(int i=0, length=companies.length; i<length; ++i) {
+		com.simulator.Company c = companies[i];
+		json += "  {\n";
+		json += "    name: " + c.getName() + ",\n";
+		json += "    symbol: " + c.getSymbol() + ",\n";
+		json += "    stockValue: " + c.getStockValue() + ",\n";
+		json += "    availableStocks: " + c.getNumberOfAvailableStocks() + "\n";
+		json += "  }";
+		if(i<length-1)
+		    json += ",";
+		json += "\n";
 	    }
-
-	    String json = "{\n";
-	    json += "  name: " + c.getName() + ",\n";
-	    json += "  symbol: " + c.getSymbol() + ",\n";
-	    json += "  stockValue: " + c.getStockValue() + ",\n";
-	    json += "  availableStocks: " + c.getNumberOfAvailableStocks() + "\n";
-	    json += "}";
+	    json += "]";
 	    return Response.ok(json, MediaType.APPLICATION_JSON).build();
 	}
 
-	@PUT
-	    @Produces(MediaType.APPLICATION_JSON)
+	@POST
 	    @Consumes(MediaType.APPLICATION_JSON)
-	    public Response updateCompany( @PathParam("symbol") String sym, InputStream data) {
-	    com.simulator.Company c = com.simulator.Company.getCompanyBySymbol(sym);
-	    if(c == null) {
-		return Response.status(Response.Status.NOT_FOUND).build();
-	    }
-	    
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response postCompany(InputStream data) {
+
 	    String json = "";
 	    try {
 		BufferedReader in = new BufferedReader(new InputStreamReader(data));
 		String line = null;
 		while((line = in.readLine()) != null)
 		    json += line;
-
+		
 		JSONObject obj = new JSONObject(json);
 		String name = obj.getString("name");
-		
-		// Attempt to update the company
+		String symbol = obj.getString("symbol");
+
+		com.simulator.Company c = com.simulator.Company.getCompanyBySymbol(symbol);
+		if(c == null) {
+		    return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+
+		// Attempt to add the company
 		// If it fails, return a server error
-		if(!com.simulator.Company.updateCompany(name, sym)) {
+		if(!com.simulator.Company.addCompany(name, symbol)) {
 		    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 		}
-		
+
 	    } catch(IOException | JSONException e) { // bad json format
 		return Response.status(Response.Status.BAD_REQUEST).build();
 	    }
-	    return Response.status(Response.Status.OK).build();
+	    return Response.status(Response.Status.CREATED).build();
 	}
 	
-	@DELETE
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public Response deleteCompany( @PathParam("symbol") String sym ) {
-	    com.simulator.Company c = com.simulator.Company.getCompanyBySymbol(sym);
-	    if(c == null) {
-		return Response.status(Response.Status.NOT_FOUND).build();
-	    }
-
-	    // Attempt to add the company
-	    // If it fails, return a server error
-	    if(!com.simulator.Company.deleteCompany(sym)) {
-		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-	    }
-	    return Response.status(Response.Status.OK).build();
-	}
     }
