3251e3561377b9f525d52239edf9180d10f6cefb
Stephen DiCerce <sdicerce@oswego.edu>
2017-03-21 15:57:11 -0400

    updated UsersRest with sorting methods

--- WEB-INF/classes/com/restful/UsersRest.java
+++ WEB-INF/classes/com/restful/UsersRest.java
@@ -3,6 +3,7 @@ package com.restful;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import com.simulator.User;
+import com.simulator.Company;
 import org.json.JSONObject;
 import org.json.JSONException;
 import java.io.BufferedReader;
@@ -15,37 +16,236 @@ import java.io.IOException;
 	
 	@GET
 	    @Produces(MediaType.APPLICATION_JSON)
-	    public Response getUsers(@Context UriInfo uriInfo) {
+	    public Response getUsers(@Context UriInfo uriInfo, @QueryParam("sort") String sortingMethod, @QueryParam("company") String companyName) {
 	    com.simulator.User[] users = com.simulator.User.getUsers();
+	    com.simulator.Company[] companies = com.simulator.Company.getCompanies();
+	    com.simulator.User temp;
+	    String json = "";
 	    if(users == null)
 		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-
-	    String json = "[\n";
-	    for(int i = 0, length = users.length; i < length; ++i) {
-		com.simulator.User u = users[i];
-		UriBuilder builder = uriInfo.getAbsolutePathBuilder();
-		String currentPath = builder.build().toString();
-		String uname = u.getName();
-		json += "  {\n";
-		json += "    \"name\": \"" + uname + "\",\n";
-		json += "    \"password\": \"" + u.getPassword() + "\",\n";
-		json += "    \"money\": " + u.getMoney() + ",\n";
-		json += "    \"stockValue\": " + u.getStockValue() + ",\n";
-		json += "    \"links\": [\n";
-		json += "      {\n";
-		json += "        \"rel\": \"self\",\n";
-		json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
-		json += "      }\n";
-		json += "    ]\n";
-		json += "  }";
-		if(i < length - 1)
-		    json += ",";
-		json += "\n";
+	    if(sortingMethod != null) {
+		if(!sortingMethod.equals("stocks") || companyName == null) {
+		    if (sortingMethod.equals("money")){
+			for (int i = 0; i < users.length; ++i) {
+			    for (int j = i + 1; j < users.length; ++j) {
+				if (users[j].getMoney() > users[i].getMoney()) {
+				    temp = users[i];
+				    users[i] = users[j];
+				users[j] = temp;
+				}
+			    }
+			}
+			
+		    } else if (sortingMethod.equals("total_money")) {
+			for (int i = 0; i < users.length; ++i) {
+			    double totalMoneyI = users[i].getMoney() + users[i].getStockValue();
+			    for (int j = i + 1; j < users.length; ++j) {
+				double totalMoneyJ = users[j].getMoney() + users[j].getStockValue();
+				if (totalMoneyJ > totalMoneyI) {
+				    temp = users[i];
+				    users[i] = users[j];
+				    users[j] = temp;
+				}
+			    }
+			}
+		    }
+		    json = "[\n";
+		    for(int i = 0, length = users.length; i < length; ++i) {
+			com.simulator.User u = users[i];
+			UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			String currentPath = builder.build().toString();
+			String uname = u.getName();
+			double totalMoney = u.getMoney() + u.getStockValue();
+			json += "  {\n";
+			json += "    \"name\": \"" + uname + "\",\n";
+			json += "    \"password\": \"" + u.getPassword() + "\",\n";
+			json += "    \"money\": " + u.getMoney() + ",\n";
+			json += "    \"stockValue\": " + u.getStockValue() + ",\n";
+			json += "    \"totalMoney\": " + totalMoney + ",\n";
+			json += "    \"links\": [\n";
+			json += "      {\n";
+			json += "        \"rel\": \"self\",\n";
+			json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
+			json += "      }\n";
+			json += "    ]\n";
+			json += "  }";
+			if(i < length - 1)
+			    json += ",";
+			json += "\n";
+		    }
+		    json += "]";
+		} else if (sortingMethod.equals("stocks") && companyName != null) {
+		    for (int i = 0; i <users.length; ++i) {
+			for (int j = i + 1; j <users.length; ++j) {
+			    if (users[j].getNumberOfStocks(companyName) > users[i].getNumberOfStocks(companyName)) {
+				temp = users[i];
+				users[i] = users[j];
+				users[j] = temp;
+			    }
+			}
+		    }
+		    
+		    json = "[\n";
+		    json += "{";
+		    json += "\"users\" : [";
+		    for (int i = 0; i < users.length; ++i) {
+			com.simulator.User u = users[i];
+			com.simulator.Company company = com.simulator.Company.getCompany(companyName);
+			UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			String currentPath = builder.build().toString();
+			String uname = u.getName();
+			int row = i+1;
+			double total;
+			json += "{";
+			json += "\"name\" : \"" + u.getName() + "\",";
+			int stocks = u.getNumberOfStocks(companyName);
+			json += "\"numberStocks\" : " + stocks + ",";
+			double averagePrice = u.getAveragePriceBoughtAt(companyName);
+			json += "\"averagePrice\" : " + String.format("%.2f", averagePrice) + ",";
+			total = stocks * averagePrice;
+			json += "\"totalInvestment\" : "+ String.format("%.2f", total);
+			json +="}";
+			json += "    \"links\": [\n";
+			json += "      {\n";
+			json += "        \"rel\": \"self\",\n";
+			json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
+			json += "      }\n";
+			json += "    ]\n";
+			if (i != users.length - 1)
+			    json += ",";
+		    }
+		    json += "]\n";
+		    json += "}\n";
+		    json += "]";
+		}
+	    } else {
+		json = "[\n";
+			for(int i = 0, length = users.length; i < length; ++i) {
+			    com.simulator.User u = users[i];
+			    UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			    String currentPath = builder.build().toString();
+			    String uname = u.getName();
+			    double totalMoney = u.getMoney() + u.getStockValue();
+			    json += "  {\n";
+			    json += "    \"name\": \"" + uname + "\",\n";
+			    json += "    \"password\": \"" + u.getPassword() + "\",\n";
+			    json += "    \"money\": " + u.getMoney() + ",\n";
+			    json += "    \"stockValue\": " + u.getStockValue() + ",\n";
+			    json += "    \"totalMoney\": " + totalMoney + ",\n";
+			    json += "    \"links\": [\n";
+			    json += "      {\n";
+			    json += "        \"rel\": \"self\",\n";
+			    json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
+			    json += "      }\n";
+			    json += "    ]\n";
+			    json += "  }";
+			    if(i < length - 1)
+				json += ",";
+			    json += "\n";
+			}
+			json += "]";
 	    }
-	    json += "]";
+// 	    if ((sortingMethod.equals("money") || sortingMethod.equals("total_money") || 
+// 		 sortingMethod == null || sortingMethod.equals("")) && 
+// 		(companyName == null || companyName.equals(""))) {
+// 		if (sortingMethod.equals("money")){
+// 		    for (int i = 0; i < users.length; ++i) {
+// 			for (int j = i + 1; j < users.length; ++j) {
+// 			    if (users[j].getMoney() > users[i].getMoney()) {
+// 				temp = users[i];
+// 				users[i] = users[j];
+// 				users[j] = temp;
+// 			    }
+// 			}
+// 		    }
+		    
+// 		} else if (sortingMethod.equals("total_money")) {
+// 		    for (int i = 0; i < users.length; ++i) {
+// 			double totalMoneyI = users[i].getMoney() + users[i].getStockValue();
+// 			for (int j = i + 1; j < users.length; ++j) {
+// 			    double totalMoneyJ = users[j].getMoney() + users[j].getStockValue();
+// 			    if (totalMoneyJ > totalMoneyI) {
+// 				temp = users[i];
+// 				users[i] = users[j];
+// 				users[j] = temp;
+// 			    }
+// 			}
+// 		    }
+		    
+// 		}
+// 		json = "[\n";
+// 		for(int i = 0, length = users.length; i < length; ++i) {
+// 		    com.simulator.User u = users[i];
+// 		    UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+// 		    String currentPath = builder.build().toString();
+// 		    String uname = u.getName();
+// 		    double totalMoney = u.getMoney() + u.getStockValue();
+// 		    json += "  {\n";
+// 		    json += "    \"name\": \"" + uname + "\",\n";
+// 		    json += "    \"password\": \"" + u.getPassword() + "\",\n";
+// 		    json += "    \"money\": " + u.getMoney() + ",\n";
+// 		    json += "    \"stockValue\": " + u.getStockValue() + ",\n";
+// 		    json += "    \"totalMoney\": " + totalMoney + ",\n";
+// 		    json += "    \"links\": [\n";
+// 		    json += "      {\n";
+// 		    json += "        \"rel\": \"self\",\n";
+// 		    json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
+// 		    json += "      }\n";
+// 		    json += "    ]\n";
+// 		    json += "  }";
+// 		    if(i < length - 1)
+// 			json += ",";
+// 		    json += "\n";
+// 		}
+// 		json += "]";
+// 	    } else if(sortingMethod.equals("stocks")) {
+// 		for (int i = 0; i <users.length; ++i) {
+// 		    for (int j = i + 1; j <users.length; ++j) {
+// 			if (users[j].getNumberOfStocks(companyName) > users[i].getNumberOfStocks(companyName)) {
+// 			    temp = users[i];
+// 			    users[i] = users[j];
+// 			    users[j] = temp;
+// 			}
+// 		    }
+// 		}
+		
+// 		json = "[\n";
+// 		json += "{";
+// 		json += "\"users\" : [";
+// 		for (int i = 0; i < users.length; ++i) {
+// 		    com.simulator.User u = users[i];
+// 		    com.simulator.Company company = com.simulator.Company.getCompany(companyName);
+// 		    UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+// 		    String currentPath = builder.build().toString();
+// 		    String uname = u.getName();
+// 		    int row = i+1;
+// 		    double total;
+// 		    json += "{";
+// 		    json += "\"name\" : \"" + u.getName() + "\",";
+// 		    int stocks = u.getNumberOfStocks(companyName);
+// 		    json += "\"numberStocks\" : " + stocks + ",";
+// 		    double averagePrice = u.getAveragePriceBoughtAt(companyName);
+// 		    json += "\"averagePrice\" : " + String.format("%.2f", averagePrice) + ",";
+// 		    total = stocks * averagePrice;
+// 		    json += "\"totalInvestment\" : "+ String.format("%.2f", total);
+// 		    json +="}";
+// 		    json += "    \"links\": [\n";
+//                     json += "      {\n";
+//                     json += "        \"rel\": \"self\",\n";
+//                     json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
+//                     json += "      }\n";
+//                     json += "    ]\n";
+// 		    if (i != users.length - 1)
+// 			json += ",";
+// 		}
+// 		json += "]\n";
+// 		json += "}\n";
+// 		json += "]";
+// 	    }
+	    
 	    return Response.ok(json, MediaType.APPLICATION_JSON).build();
 	}
-
+	
 	@POST
 	    @Consumes(MediaType.APPLICATION_JSON)
 	    @Produces(MediaType.APPLICATION_JSON)
